98b6547c21914359d049c98919abff37a46e5b1c
xujie <jyoketsu@gmail.com>
2021-09-09 14:34:45 +0800

    feat: 在项目中改变 SCSS 变量自定义主题色

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import ElementPlus from "element-plus";
-import "../theme/index.css";
+// import "../theme/index.css";
+import "./assets/style/element-variables.scss";
 
 const app = createApp(App);
 app.use(store).use(router).use(ElementPlus).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 全局变量
+        additionalData: '@import "./src/assets/style/global-variables.scss";',
+      },
+    },
+  },
+});
