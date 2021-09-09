b2fa71d0da06af32964676d50e59e426433a1b45
xujie <jyoketsu@gmail.com>
2021-09-09 17:31:52 +0800

    feat: 首页布局

---
+++ src/views/home/LeftMenu.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="left-menu">
+    <div class="head">
+      <i></i>
+      <span>Working Admin</span>
+    </div>
+    <div class="menu-wrapper">
+      <el-menu
+        :uniqueOpened="true"
+        default-active="1"
+        class="el-menu-vertical"
+        @open="handleOpen"
+        @close="handleClose"
+        background-color="#474747"
+        text-color="#fff"
+        active-text-color="#1ca8b3"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <i class="el-icon-postcard"></i>
+            <span>卡片管理</span>
+          </template>
+          <el-menu-item index="1-1">卡片字段</el-menu-item>
+          <el-menu-item index="1-2">卡片类型</el-menu-item>
+        </el-sub-menu>
+      </el-menu>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {
+      handleOpen: (key: string, keyPath: string) => {
+        console.log(key, keyPath);
+      },
+      handleClose: (key: string, keyPath: string) => {
+        console.log(key, keyPath);
+      },
+    };
+  },
+});
+</script>
+<style scoped lang="scss">
+.left-menu {
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+}
+.head {
+  width: 100%;
+  height: 48px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.head > i {
+  width: 32px;
+  height: 32px;
+  background-image: url(/icon/logo.svg);
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+.head > span {
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+  white-space: nowrap;
+}
+.menu-wrapper {
+  width: 100%;
+  flex: 1;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+::-webkit-scrollbar {
+  /*滚动条整体样式*/
+  width: 5px;
+  /*高宽分别对应横竖滚动条的尺寸*/
+  height: 10px;
+}
+::-webkit-scrollbar-track {
+  /*滚动条里面轨道*/
+  box-shadow: inset 0 0 8px rgba(0, 0, 0, 0.2);
+  background: $menuBg;
+}
+::-webkit-scrollbar-thumb {
+  /*滚动条里面小方块*/
+  border-radius: 10px;
+  background-color: #1ca8b3;
+}
+.el-menu-vertical {
+  border-right: unset;
+  text-align: left;
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,25 @@
 <template>
-  <div>home</div>
+  <el-container class="root">
+    <el-aside width="200px"><leftMenu /></el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
+
 <script lang="ts">
-import { computed, defineComponent, onMounted } from "vue";
+import { defineComponent, onMounted } from "vue";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
+import LeftMenu from "./LeftMenu.vue";
 
 export default defineComponent({
+  components: { LeftMenu },
   setup() {
     const router = useRouter();
     const store = useStore();
-    
+
     onMounted(() => {
       const token = localStorage.getItem("auth_token");
       if (token) {
@@ -22,3 +31,27 @@ export default defineComponent({
   },
 });
 </script>
+<style scoped lang="scss">
+.root {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: $menuBg;
+  overflow: hidden;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  width: 100%;
+  height: 100%;
+}
+</style>
